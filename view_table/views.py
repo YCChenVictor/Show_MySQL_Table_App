c12c41a6eb43c6137af8300c580242024f950193
Victor <t5204713910@gmail.com>
2018-09-16 10:38:19 +0800

    add index for the two model

--- view_table/views.py
+++ view_table/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from view_table.models import Stocks, Alibaba_Group_Holding_Limited
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the view_table index.")
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_Stocks = Stocks.objects.all().count()
+    num_Alibaba_Group_Holding_Limited = Alibaba_Group_Holding_Limited.objects.all().count()
+
+    # Equity Stocks(status = 'e')
+    # num_equities = Stocks.objects.filter(status__exact='e').count()
+
+    context = {
+        'num_Stocks': num_Stocks,
+        'num_Alibaba_Group_Holding_Limited': num_Alibaba_Group_Holding_Limited,
+        # 'num_equities': num_equities,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
