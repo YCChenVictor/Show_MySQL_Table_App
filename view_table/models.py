df4e465f05e6c6fe22dc070344ff264a51f8f82e
Victor <t5204713910@gmail.com>
2018-09-16 10:36:04 +0800

    construct two models

--- view_table/models.py
+++ view_table/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+"""
+first thing frist, construct a class for all the stock name in MySQL
+"""
+
+
+# the table of all the stock names
+class Stocks(models.Model):
+    nane = models.CharField(u'Name', max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+# the Alibaba_Group_Holding_Limited table
+class Alibaba_Group_Holding_Limited(models.Model):
+    table = models.TextField(u'Content')
+    stock = models.ForeignKey('Stocks', blank=True, null=True,
+                              on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.table
