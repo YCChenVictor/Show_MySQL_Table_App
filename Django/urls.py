b31d2d4559d9dd658d4c88ce1eada7a9d3a6fd17
Victor <t5204713910@gmail.com>
2018-09-18 10:30:41 +0800

    use other reference
    I use the mozilla's resource to rebuild the app for its more clear and step by step procedure. The
    new context includes a gradually added codes to setup urls.py.

--- Django/urls.py
+++ Django/urls.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('view_table/', include('view_table.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+# Use include() to add paths from the application
+from django.conf.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('view_table/', include('view_table.urls')),
+]
+
+
+# Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/view_table/')),
+]
+
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
