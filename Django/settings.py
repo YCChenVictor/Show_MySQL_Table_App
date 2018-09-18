1869d26e078cd58c9a43facccb087495645945c2
Victor <t5204713910@gmail.com>
2018-09-18 10:29:32 +0800

    correct the app name

--- Django/settings.py
+++ Django/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'view_table'
+    'view_table.apps.ViewTableConfig'
 ]
 
 MIDDLEWARE = [
