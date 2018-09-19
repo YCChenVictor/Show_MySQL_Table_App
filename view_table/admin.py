1a02f75301c9f57cad1c02e0768a27d740529ad8
Victor <t5204713910@gmail.com>
2018-09-19 10:15:48 +0800

    register new models

--- view_table/admin.py
+++ view_table/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
+from view_table.models import Genre, instrument
 
-from view_table.models import Stocks, Alibaba_Group_Holding_Limited
-
-admin.site.register(Stocks)
-admin.site.register(Alibaba_Group_Holding_Limited)
+admin.site.register(Genre)
+admin.site.register(instrument)
