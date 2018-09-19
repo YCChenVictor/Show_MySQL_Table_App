b0c7936d4b8da71ea29fd2f88ce6b8738afd84ac
Victor <t5204713910@gmail.com>
2018-09-19 10:17:14 +0800

    restructure the script for more readability

--- view_table/models.py
+++ view_table/models.py
@@ -3,31 +3,52 @@ from django.db import models
 
 class Genre(models.Model):
     """Model representing a finance instrument genre."""
-    name = models.CharField(max_length=200,
-                            help_text='Enter a instrument genre (e.g. Equity)')
+    name = models.CharField(max_length=50,
+                            help_text=('Enter an instrument genre e.g.' +
+                                       ' Equity,' +
+                                       ' Futures,' +
+                                       ' ETF,' +
+                                       ' Fund' +
+                                       ' Index' +
+                                       ' Option'))
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
 
-from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+# Used to generate URLs by reversing the URL patterns
+from django.urls import reverse
 
-class Instrument(models.Model):
-    """Model representing a instrument(but not a specific copy of a instrumnet)."""
-    # id = models.ForeignKey('id', on_delete=models.SET_NULL, null=True)
+
+class instrument(models.Model):
+    """
+    Model representing a instrument(but not a specific copy of a instrumnet).
+    The column, id, will be automatically created.
+    """
     ticker = models.CharField(max_length=50)
-    name = models.TextField(max_length=50, help_text='Enter a of the instrument')
-    exchange = models.CharField('Exchange', max_length=13, help_text='The name of exchange institution')
-    Type = models.CharField('Type', max_length=1, help_text='S for stock; E for Equity ...etc')
-    # ManyToManyField used because genre can contain many instruments. Instruments can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    genre = models.ManyToManyField(Genre, help_text='Select a genre for this instrument')
+
+    name = models.TextField(max_length=50,
+                            help_text='Enter the name of an instrument')
+
+    exchange = models.CharField('Exchange', max_length=13,
+                                help_text='The name of exchange institution')
+
+    # Type = models.CharField('Type', max_length=1,
+    #                         help_text='S for stock; E for Equity ...etc')
+
+    genre = models.ManyToManyField(Genre,
+                                   help_text='Select a genre for this instrument')
+    """
+    ManyToManyField used because genre can contain many instruments.
+    Instruments can cover many genres.
+    Genre class has already been defined so we can specify the object above.
+    """
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
     def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('instrument-detail', args=[str(self.id)])
+        """Returns the url to access a detail record for this Instrument."""
+        return reverse('instrument-detail', args=[str(self.ticker)])
