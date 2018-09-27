9d30df9a2bd212cbd04ef7748649d5eb712a0916
Victor <t5204713910@gmail.com>
2018-09-27 15:16:09 +0800

    successfully display table's context with row by row

--- Django/settings.py
+++ Django/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Show_Table.apps.ShowTableConfig'
+    'Show_Table.apps.ShowTableConfig',
 ]
 
 MIDDLEWARE = [
